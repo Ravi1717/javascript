52fe04eda24852ef47863487703e5d19c602a5e3
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2020-01-15 01:20:44 +0530

    destruturing in es6

---
+++ destructuing.js
@@ -0,0 +1,12 @@
+/*var object={one:1,two:2,three:3};
+one=object.one;
+two=object.two;
+three=object.three;
+console.log(one,two,three);
+*/
+
+let object = { one: 1, two: 2, three: 3 };
+let { one, two, three } = object;
+console.log(one);
+console.log(two);
+console.log(three);
--- reversestrng.js
+++ reversestrng.js
@@ -1,3 +1,4 @@
+/*
 var name='Ravi';
 String.prototype.reversed=function(){
     var r='';
@@ -7,3 +8,78 @@ String.prototype.reversed=function(){
     return r;
 }
 console.log(name.reversed());
+*/
+/*
+function parentfunc(){
+    var a=4554;
+function nestedfunc(){
+    var b=2000;
+    return a+b;
+}
+return nestedfunc();
+};
+var res=parentfunc();
+console.log(res);
+*/
+/*
+function makeadder(a){
+    return function(b){
+        return a+b;
+    };
+}
+var add5=makeadder(5);
+var add20=makeadder(20);
+console.log(add20);
+*/
+
+/*
+function sayHello(name){
+    var text='hello' + name;
+    var say=function(){
+        console.log(text);
+    }
+    say();
+    }
+    sayHello('ravi');
+*/
+
+function one(){
+    var x=12;
+    return function(){
+        var y=12;
+        console.log(x+y);
+    }
+}
+var res=one();
+res();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
