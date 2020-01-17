bf9facda8881f6387d675224798d5eee4eda724f
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2020-01-17 21:22:48 +0530

    iife function

---
+++ iife.js
@@ -0,0 +1,5 @@
+var x = 15;
+var y = 10;
+(function (x1, y1) {
+    console.log("substraction = " + (x1 - y1));
+})(x, y);
---
+++ returnparamiife.js
@@ -0,0 +1,6 @@
+var result = (function () {
+    var name = 'ravi';
+    return name;
+})();
+var s = result;
+console.log(s);
