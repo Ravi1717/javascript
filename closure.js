77c7d76dcf373e6522c8968b3f354e811f5672cc
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2020-01-20 23:30:39 +0530

    higher order function

---
+++ closure.js
@@ -0,0 +1,13 @@
+function makeCounter() {
+    let count = 0;
+
+    return function () {
+        return count++;
+    };
+}
+
+let counter = makeCounter();
+
+alert(counter());
+alert(counter());
+alert(counter()); 
---
+++ object.js
@@ -0,0 +1,9 @@
+var obj = {
+    name: 'ravi',
+    age: 22,
+    function: function (test) {
+        console.log(test);
+    },
+    arr: [22, 'rohit', 01, "shamu"],
+}
+console.log(obj.arr);
