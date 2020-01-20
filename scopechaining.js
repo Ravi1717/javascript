fd12f976c9489faf84dc8a4a3f995c99cf0f3c43
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2020-01-20 23:38:55 +0530

    scope chain

---
+++ scopechaining.js
@@ -0,0 +1,11 @@
+let foo = 'foo';
+function bar() {
+    let baz = 'baz';
+    // Prints 'baz'
+    console.log(baz);
+    // Prints 'foo'
+    console.log(foo);
+    number = 42;
+    console.log(number);  // Prints 42
+}
+bar();
